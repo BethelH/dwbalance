e7f4fd858ee3bae11dd8782658f0b579abeb5fac
Bethel Hailu <bethel.hailu@neofinancial.com>
2023-07-07 05:54:35 -0600

    Working copy of the balance checking part

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,40 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import sortby from 'lodash.sortby';
 import { Box } from '@mui/material'
 
 import "@fontsource/roboto";
 import "@fontsource/roboto-mono";
 
-import { BalanceChecker } from './components/balance-checker/BalanceChecker';
+import { BalanceCheckerForm } from './components/balance-checker/BalanceCheckerForm';
 // import {BalanceList} from './components/balance-checker/balance-list/BalanceList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [balances, setBalances] = useState([]);
 
-    this.state = {
-      balances: [],
-    };
+  useEffect(() => {
+    console.log('balancesb4',JSON.stringify(balances))
 
-    this.checkCard = this.checkCard.bind(this)
-  }
+    setBalances(sortby(balances, 'balance'));
+    console.log('balances',JSON.stringify(balances))
+  }, [balances]);
 
-  //2222222222222222
-  async checkCard(cardNumber) {
-    const currentBalances = this.state.balances;
-    console.log('currentBalances',currentBalances.length)
-    await fetch("http://localhost:8000/determineBalanceApi/" + cardNumber)
-      .then(res => res.text())
-      .finally(res => this.setState({ balances: currentBalances.push({ cardNumber, balance: parseInt(res) }) }))
+  //2223222222222225
+  //2223222222222223
+  //1111222222222222
+  //1111222222222221
+  async function  checkCard (cardNumber) {
+     const response = await fetch("http://localhost:8000/determineBalanceApi/" + cardNumber)
+    const balance = await response.text()
 
-      console.log('afterBalance', this.state.balances.length)
+    setBalances([...balances, { cardNumber, balance: parseInt(balance) }])
+  };
 
-  }
-
-
-  render() {
-    return (
-      <Box >
-        <BalanceChecker onClick={this.checkCard} />
-        {/* <BalanceList balances={this.state.balances} /> */}
-      </Box>
-    )
-  }
+  return (
+    <Box >
+      <BalanceCheckerForm onClick={checkCard} />
+      {/* <BalanceList balances={balances} /> */}
+    </Box>
+  )
 }
 
 export default App;
